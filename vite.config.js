1fbcc1d3df18976273cd32d893d01fff229dd990
damilola1ade <damiiyi@gmail.com>
2023-08-23 10:55:53 +0100

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode, ...rest }) => {
+  const defaultOption = {
+    esbuild: {
+      loader: "jsx",
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          ".js": "jsx",
+          ".ts": "tsx",
+        },
+      },
+    },
+    plugins: [react()],
+  };
+  if (command === "build") {
+    return {
+      build: {
+        commonjsOptions: {
+          transformMixedEsModules: true,
+          exclude: [ 'node_modules/lodash-es/**', 'node_modules/@types/lodash-es/**', ], //This fixes exports at lodash error
+          ignoreTryCatch: false
+        },
+        rollupOptions: {
+          output: {
+            manualChunks(id) {
+              if (id.includes("node_modules")) {
+                return id
+                  .toString()
+                  .split("node_modules/")[1]
+                  .split("/")[0]
+                  .toString();
+              }
+            },
+          },
+        },
+      },
+      ...defaultOption,
+    };
+  } else {
+    return defaultOption;
+  }
+});
